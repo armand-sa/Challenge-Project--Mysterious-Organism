bfee8217177a0bdffea11c55405238d70c142eee
Armand McKey <armand@armandvandermerwe.com>
2024-10-05 11:22:25 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-// * START:
+// * Starter Code:
 
 // Returns a random DNA base
 const returnRandBase = () => {
   const dnaBases = ['A', 'T', 'C', 'G']
   return dnaBases[Math.floor(Math.random() * 4)] 
 }
+// console.log(returnRandBase())
 
 // Returns a random single strand of DNA containing 15 bases
 const mockUpStrand = () => {
@@ -14,3 +15,7 @@ const mockUpStrand = () => {
   }
   return newStrand
 }
+// console.log(mockUpStrand())
+
+
+// * Your Code:
