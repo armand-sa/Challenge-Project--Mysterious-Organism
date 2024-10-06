ae5f853865fa4ed727fe53bf928c80bd97be68e8
Armand McKey <armand@armandvandermerwe.com>
2024-10-06 10:30:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -19,3 +19,16 @@ const mockUpStrand = () => {
 
 
 // * Your Code:
+
+/*
+! Task 1
+* Since you need to create multiple objects, create a factory function pAequorFactory() that has two parameters:
+
+  * - The first parameter is a number (no two organisms should have the same number).
+  *  -The second parameter is an array of 15 DNA bases.
+* pAequorFactory() should return an object that contains the properties specimenNum and dna that correspond to the parameters provided.
+
+* You’ll also add more methods to this returned object in the later steps.
+*/
+
+// Tasks 1:
